af7f48c0e78f5e650cbcdd097d14597daf1b9979
liltheu <50384574+liltheu@users.noreply.github.com>
2021-12-08 22:32:50 -0300

    #3 Finalização Jogo Wizard Number logica, inicio criação interface
    Foi finalizado o jogo em c# e unity, agora estou iniciando a parte de interface onde o usuario irá ver o game, com personagens, balão de fala , e botões de interação.

--- Wizard Number Game/Assets/WizardNumber.cs
+++ Wizard Number Game/Assets/WizardNumber.cs
@@ -4,31 +4,55 @@ using UnityEngine;
 
 public class WizardNumber : MonoBehaviour
 {
+    int max;
+    int min;
+    int numeroAdvinhado;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
-        int numero = (max+min/2);
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        numeroAdvinhado = 500;
+
         Debug.Log("Bem vindo ao Advinhador de Numeros !");
         Debug.Log("Pense em um numero, e não me fale qual é !");
-        Debug.Log("O maior numero que você pode pensar é :" +max);
-        Debug.Log("O menor numero que você pode pensar é :" +min);
-        Debug.Log("Me diz se o numero que você pensou é maior ou menor que : " +numero);
+        Debug.Log("O maior numero que você pode pensar é :" + max);
+        Debug.Log("O menor numero que você pode pensar é :" + min);
+        Debug.Log("Me diz se o numero que você pensou é maior ou menor que : " + numeroAdvinhado);
         Debug.Log("Aperte para cima se = Maior, Aperte para baixo se = Menor, e Enter se = Correto!");
-
+        max = max + 1;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Seta Para cima foi apertada!");
+            min = numeroAdvinhado;
+            ProximoAdvinha();
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Seta Para baixo foi apertada!");
+            max = numeroAdvinhado;
+            numeroAdvinhado = (max + min) / 2;
+            ProximoAdvinha();
         }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Eu acertei o numero é : " + numeroAdvinhado + " !!");
+            StartGame();
+        }
+    }
+    void ProximoAdvinha()
+    {
+        numeroAdvinhado = (max + min) / 2;
+        Debug.Log("É maior ou menor que .." + numeroAdvinhado);
     }
 }
